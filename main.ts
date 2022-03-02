cb174db65220cbe42639164b9c4ebfcb435cc47c
MartinReinhart <reinhart19_91@hotmail.com>
2022-03-02 19:34:22 +0100

    added search everywhere function

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Scope, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -6,8 +6,55 @@ interface MyPluginSettings {
 	mySetting: string;
 }
 
+
+
+
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
+}
+
+let pressed;
+let lastPressed: any;
+let isDoublePress: boolean;
+
+const handleDoublePresss = (key: { keyCode?: any; key?: any; })=> {
+    console.log(key.key, 'pressed two times');
+	searchEverywhere();
+}
+
+const timeOut = () => setTimeout(() => isDoublePress = false, 500);
+
+const keyPress = (key: { keyCode?: any; key?: any; }) => {
+    pressed = key.keyCode;
+    if (isDoublePress && pressed === lastPressed) {
+        isDoublePress = false;
+        handleDoublePresss(key);
+    } else {
+        isDoublePress = true;
+        timeOut();
+    }
+    lastPressed = pressed;
+}
+
+function searchEverywhere(){
+	//Press STRG+SHIFT+F
+	window.dispatchEvent(new KeyboardEvent('keydown', {
+		key: "f",
+		keyCode: 70,
+		code: "KeyE",
+		which: 70,
+		shiftKey: true,
+		ctrlKey: true,
+		metaKey: false
+	  }));
+}
+
+function addDoubleKeypressEventListener() {
+	window.addEventListener('keydown', (e) => {
+		if (e.keyCode == 16) {
+			keyPress(e);
+		}
+	});
 }
 
 export default class MyPlugin extends Plugin {
@@ -15,65 +62,9 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
+		addDoubleKeypressEventListener();
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
+		//this.addSettingTab(new SampleSettingTab(this.app, this));
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
@@ -135,3 +126,5 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+
+
